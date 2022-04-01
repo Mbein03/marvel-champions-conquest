b2e5e4c7124b1114ecff918057f4590034c036cb
Marc Bein <mbein3@gmail.com>
2022-04-01 17:54:00 -0400

    Store updates.

--- client/src/components/common/Image.jsx
+++ client/src/components/common/Image.jsx
@@ -1,3 +1,10 @@
-export const Image = ({ src, alt }) => {
-  return <img className='py-3 my-2' src={src} alt={alt}></img>;
+import classNames from 'classnames';
+export const Image = ({ src, alt, storeCard }) => {
+  const imageClass = classNames({
+    'py-3': true,
+    'my-2': true,
+    'h-[27.5rem]': storeCard,
+  });
+
+  return <img className={imageClass} src={src} alt={alt}></img>;
 };
